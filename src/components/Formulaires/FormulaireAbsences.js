6cb40c4514003c3648c1492ab9480f75eceb322d
peyau <jonathan.levaux.7@hotmail.com>
2023-08-08 11:50:27 +0200

    wip formulaire absence

---
+++ src/components/Formulaires/FormulaireAbsences.js
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+
+const FormulaireAbsences = () => {
+  const [absenceType, setAbsenceType] = useState("conge");
+  const [absenceDate, setAbsenceDate] = useState("");
+  const [absenceDuration, setAbsenceDuration] = useState("full-day");
+  const [startTime, setStartTime] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [reason, setReason] = useState("");
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    // Gérer la soumission du formulaire ici
+  };
+
+  return (
+    <div className="form-container">
+      <form className="generic-form" onSubmit={handleFormSubmit}>
+        <label htmlFor="absence-type">Type d'absence :</label>
+        <select
+          id="absence-type"
+          className="generic-select"
+          value={absenceType}
+          onChange={(e) => setAbsenceType(e.target.value)}
+        >
+          <option value="conge">Congé</option>
+          <option value="conge-social">Congé Social</option>
+          <option value="maladie">Maladie</option>
+        </select>
+        <label htmlFor="absence-date">Date d'absence :</label>
+        <input
+          type="date"
+          id="absence-date"
+          className="generic-input"
+          value={absenceDate}
+          onChange={(e) => setAbsenceDate(e.target.value)}
+          required
+        />
+        <label htmlFor="absence-duration">Durée :</label>
+        <select
+          id="absence-duration"
+          className="generic-select"
+          value={absenceDuration}
+          onChange={(e) => setAbsenceDuration(e.target.value)}
+        >
+          <option value="half-day">Demi-journée</option>
+          <option value="full-day">Journée complète</option>
+          <option value="multiple-days">Plusieurs jours</option>
+        </select>
+        {absenceDuration === "half-day" && (
+          <div className="time-input-container">
+            <label htmlFor="absence-start-time">Heure de début :</label>
+            <input
+              type="time"
+              id="absence-start-time"
+              className="generic-input"
+              value={startTime}
+              onChange={(e) => setStartTime(e.target.value)}
+            />
+          </div>
+        )}
+        <div className="date-input-container">
+          <label htmlFor="absence-start-date">Date de début :</label>
+          <input
+            type="date"
+            id="absence-start-date"
+            className="generic-input"
+            value={startDate}
+            onChange={(e) => setStartDate(e.target.value)}
+            required
+          />
+        </div>
+        {absenceDuration === "multiple-days" && (
+          <div className="date-input-container">
+            <label htmlFor="absence-end-date">Date de fin :</label>
+            <input
+              type="date"
+              id="absence-end-date"
+              className="generic-input"
+              value={endDate}
+              onChange={(e) => setEndDate(e.target.value)}
+            />
+          </div>
+        )}
+        <div className="generic-input-container">
+          <label htmlFor="reason" className="generic-label">
+            Raison :
+          </label>
+          <input
+            type="text"
+            id="reason"
+            className="generic-input"
+            value={reason}
+            onChange={(e) => setReason(e.target.value)}
+            required
+          />
+        </div>
+
+        <button type="submit" className="modal-button">
+          Soumettre
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default FormulaireAbsences;
--- src/pages/Encodage/Absences.js
+++ src/pages/Encodage/Absences.js
@@ -1,6 +1,7 @@
 import React from "react";
 import NavBarGlobal from "../../components/NavBarGlobal";
 import NavBarEncodage from "../../components/NavBarEncodage";
+import FormulaireAbsences from "../../components/Formulaires/FormulaireAbsences";
 
 const Absences = () => {
   return (
@@ -8,6 +9,7 @@ const Absences = () => {
       <NavBarGlobal />
       <h1>Encodage des absences</h1>
       <NavBarEncodage />
+      <FormulaireAbsences />
     </div>
   );
 };
