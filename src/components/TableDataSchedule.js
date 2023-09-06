925059db133ec72b29481db9a571ca91d471f87c
peyau <jonathan.levaux.7@hotmail.com>
2023-09-06 16:14:03 +0200

    tableau horaire

---
+++ src/components/TableDataSchedule.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+const TableDataSchedule = ({ data, className }) => {
+  if (!data || data.length === 0) {
+    return <p>Aucune donnée à afficher.</p>;
+  }
+
+  const joursSemaine = Object.keys(data[0].semaine1);
+
+  return (
+    <table className={`${className}`}>
+      <thead>
+        <tr>
+          <th></th>
+          {joursSemaine.map((jour, index) => (
+            <th key={index}>{jour}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((personne, rowIndex) => (
+          <React.Fragment key={rowIndex}>
+            <tr>
+              <td className="personne-nom">{personne.nom}</td>
+            </tr>
+            <tr className="semaine1">
+              <td></td>
+              {joursSemaine.map((jour, colIndex) => (
+                <td key={colIndex}>
+                  {personne.semaine1[jour]}
+                </td>
+              ))}
+            </tr>
+            <tr className="semaine2">
+              <td></td>
+              {joursSemaine.map((jour, colIndex) => (
+                <td key={colIndex}>
+                  {personne.semaine2[jour]}
+                </td>
+              ))}
+            </tr>
+          </React.Fragment>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default TableDataSchedule;
--- src/pages/infosPratiques/Horaires.js
+++ src/pages/infosPratiques/Horaires.js
@@ -2,27 +2,47 @@ import React from "react";
 import NavBarGlobal from "../../components/navbars/NavBarGlobal";
 import NavBarInfosPratiques from "../../components/navbars/NavBarInfosPratiques";
 import AnimatedContainer from "../../components/AnimatedContainer";
-import TableData from "../../components/TableData";
+import TableDataSchedule from "../../components/TableDataSchedule";
 
 const Horaires = () => {
   const horaire = [
     {
-      nom: "Jonathan",
-      lundi: "8h30-12h\n13h-17h",
-      mardi: "10h-13h",
-      mercredi: "10h-13h",
-      jeudi: "10h-13h",
-      vendredi: "10h-13h",
-      samedi: "10h-13h",
+      nom: "Personne 1",
+      semaine1: {
+        lundi: "9:00 - 17:00",
+        mardi: "9:00 - 17:00",
+        mercredi: "9:00 - 17:00",
+        jeudi: "9:00 - 17:00",
+        vendredi: "9:00 - 17:00",
+        samedi: "9:00 - 13:00",
+      },
+      semaine2: {
+        lundi: "10:00 - 18:00",
+        mardi: "10:00 - 18:00",
+        mercredi: "10:00 - 18:00",
+        jeudi: "10:00 - 18:00",
+        vendredi: "10:00 - 18:00",
+        samedi: "10:00 - 14:00",
+      },
     },
     {
-      nom: "Jonathan",
-      lundi: "10h-13h",
-      mardi: "10h-13h",
-      mercredi: "10h-13h",
-      jeudi: "10h-13h",
-      vendredi: "10h-13h",
-      samedi: "10h-13h",
+      nom: "Personne 2",
+      semaine1: {
+        lundi: "8:30 - 16:30",
+        mardi: "8:30 - 16:30",
+        mercredi: "8:30 - 16:30",
+        jeudi: "8:30 - 16:30",
+        vendredi: "8:30 - 16:30",
+        samedi: "8:30 - 12:30",
+      },
+      semaine2: {
+        lundi: "9:30 - 17:30",
+        mardi: "9:30 - 17:30",
+        mercredi: "9:30 - 17:30",
+        jeudi: "9:30 - 17:30",
+        vendredi: "9:30 - 17:30",
+        samedi: "9:30 - 13:30",
+      },
     },
   ];
 
@@ -33,7 +53,7 @@ const Horaires = () => {
       <NavBarInfosPratiques />
       <AnimatedContainer containerClass="sub-container">
         <h2>L'horaire de l'équipe</h2>
-        <TableData data={horaire} className="table-data" />
+        <TableDataSchedule data={horaire} className="table-data-schedule" />
       </AnimatedContainer>
     </div>
   );
