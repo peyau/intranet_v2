5a6b703659cb90f28bec4fd49850601f57b9f1da
peyau <jonathan.levaux.7@hotmail.com>
2023-10-10 17:00:37 +0200

    container page perso

--- src/pages/PagePerso.js
+++ src/pages/PagePerso.js
@@ -1,11 +1,15 @@
 import React from "react";
 import NavBarGlobal from "../components/navbars/NavBarGlobal";
+import AnimatedContainer from "../components/AnimatedContainer";
 
 const PagePerso = () => {
   return (
     <div>
       <NavBarGlobal />
       <h1>Page perso</h1>
+      <AnimatedContainer containerClass="sub-container">
+        <h2>Page personelle</h2>
+      </AnimatedContainer>
     </div>
   );
 };
